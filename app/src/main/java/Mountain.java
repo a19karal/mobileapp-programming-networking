d37b2f92eb8205a8f3670f0e611c87c87dc8995e
Karim <DITT EPOST SOM DU HAR P DITT GITHUBKONTO>
2020-05-28 14:12:41 +0200

    poop 1

---
+++ app/src/main/java/Mountain.java
@@ -0,0 +1,17 @@
+public class Mountain
+{
+    private String name;
+    private String location;
+    private int size;
+
+
+    public Mountain( String inName, String inLocation, int inSize)
+    {
+        name=inName;
+        location=inLocation;
+        size=inSize;
+
+    }
+
+
+}
